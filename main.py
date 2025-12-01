c67ddcf88ca47156812c7674a71d760dcd255a2d
Abdulrehman Seyal <abdulrehmanseyal4@gmail.com>
2025-12-01 17:09:20 +0500

    Reflection agent complete

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from chains import reflect_chain, generate_chain
 
 load_dotenv()
 
+
 class MessageGraph(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
 
@@ -30,16 +31,21 @@ builder.add_node(GENERATE, generation_node)
 builder.add_node(REFLECT, reflection_node)
 builder.set_entry_point(GENERATE)
 
+
 def should_continue(state: MessageGraph):
     if len(state["messages"]) >= 5:
         return END
     return REFLECT
 
-builder.add_conditional_edges(GENERATE, should_continue, path_map={END:END, REFLECT:REFLECT})
+
+builder.add_conditional_edges(
+    GENERATE, should_continue, path_map={END: END, REFLECT: REFLECT}
+)
 builder.add_edge(REFLECT, GENERATE)
 
 graph = builder.compile()
 print(graph.get_graph().draw_mermaid())
+
 
 def main():
     print("Hello from reflection-agent!")
